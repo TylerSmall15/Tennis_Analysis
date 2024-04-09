7b7e0876260c8ba72924572a4013e92fc1cb25c3
TylerTheCreator <TylerTheCreator@github.com>
2024-04-08 22:12:54 -0400

    first commit

---
+++ yolo_inference.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+
+model = YOLO('yolov8x')
+
+result = model.predict('input_videos/CocoGauffScreenshot.png', save=True)
+
+print(result)
+print("boxes: ")
+
+
+for box in result[0].boxes:
+    print(box)
